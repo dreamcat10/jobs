592a0b1b584e1826bec1146e16984f456601e6a7
dreamcat10 <103232329+dreamcat10@users.noreply.github.com>
2023-09-11 14:18:29 +0800

    Update Aliyun.java

--- src/main/java/com/yyoung/jobs/common/Aliyun.java
+++ src/main/java/com/yyoung/jobs/common/Aliyun.java
@@ -2,13 +2,13 @@ package com.yyoung.jobs.common;
 
 public class Aliyun {
 
-    public static final String ACCESS_KEY_ID = "LTAI5tDFcQDiJywT73Sf6oUu";
-    public static final String ACCESS_KEY_SECRET = "VaouT9qqez3O6ogY4kKplq2yhxQDYx";
-    public static final String ENDPOINT = "https://oss-cn-chengdu.aliyuncs.com";
-    public static final String BUCKETNAME = "cat-jobs";
+    public static final String ACCESS_KEY_ID = "******";
+    public static final String ACCESS_KEY_SECRET = "******";
+    public static final String ENDPOINT = "******";
+    public static final String BUCKETNAME = "******";
 
     //短信签名
     public static final String SIGN_NAME = "求职平台系统";
     //短信模板号
-    public static final String TEMPLATE_CODE = "SMS_267145453";
+    public static final String TEMPLATE_CODE = "***";
 }
